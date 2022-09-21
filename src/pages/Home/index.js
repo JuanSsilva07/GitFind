eb820a7063bfe2ccf663f7ee00b8cbdf8f2df2ca
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-09-21 14:52:09 -0300

    commit project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,79 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import background from "../../assets/half-background.png";
+import ItemList from "../../components/ItemList";
+import "./styles.css";
+
+function App() {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(
+        `https://api.github.com/users/${user}/repos`
+      );
+      const newRepos = await reposData.json();
+
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="conteudo">
+        <img src={background} alt="background app" className="background" />
+        <div className="info">
+          <div>
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@Username"
+            />
+            <button onClick={handleGetData}>Search</button>
+          </div>
+          {currentUser?.name ? (
+            <>
+              <div className="perfil">
+                <img
+                  src={currentUser.avatar_url}
+                  alt="profile image"
+                  className="profile"
+                />
+                <div>
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+              <hr />
+            </>
+          ) : null}
+          {repos?.length ? (
+            <div>
+              <h4 className="repositorio">Repository</h4>
+              {repos.map((repo) => (
+                <ItemList title={repo.name}description={repo.description} />
+              ))}
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
